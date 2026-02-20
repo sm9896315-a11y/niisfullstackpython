f86832abd10db73d2bb91c52022a55e7c9221198
saswati <sm9896315@gmail.com>
2026-02-20 12:34:46 -0800

    rectangle and square program

---
+++ pythonniis/basic program/rectangle1.py
@@ -0,0 +1,13 @@
+"""#wap take a rectangle length and breadth from keyboard """
+"""display length breadth area , perimeter """
+
+print("enter rectangle length")
+L=int(input())
+print("enter rectangle breadth")
+B=int(input())
+ar=(L*B)
+pr=2*(L+B)
+print("length=",L)
+print("breadth=",B)
+print("area=",ar)
+print("perimeter=",pr)
---
+++ pythonniis/basic program/square.py
@@ -0,0 +1,5 @@
+print("enter square side")
+S=int(input())
+ar=(S*S)
+print("side=",S)
+print("area=",ar)
