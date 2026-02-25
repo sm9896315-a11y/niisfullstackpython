f95b415825df6165bb3134577817eb8bd9e20e9e
saswati <sm9896315@gmail.com>
2026-02-24 22:01:02 -0800

    looping

--- pythonniis/switch/demo1.py
+++ pythonniis/switch/demo1.py
@@ -2,7 +2,7 @@ print("enter two nos")
 no1=int(input())
 no2=int(input())
 print("enter your choice\n1.add\n2.sub\n3.mult")
-ch=int(input())
+ch=int(input()) 
 match ch:
 	case 1:print("sum=",no1+no2)
 	case 2:print("sub=",no1-no2)
