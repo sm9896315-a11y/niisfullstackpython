5e32c186e86f749379d770dbf6a423d98d6e3aa4
saswati <sm9896315@gmail.com>
2026-02-25 20:17:33 -0800

    looping

---
+++ pythonniis/looping/demo2.py
@@ -0,0 +1,11 @@
+i=1
+print("A")
+while i<5:
+	print("B")
+	i=i+1
+	print(i)
+	if i==3:
+		break
+	print("C")
+print("D")
+print(i)		
---
+++ pythonniis/looping/demo5.py
@@ -0,0 +1,8 @@
+i=1
+while i<4:
+	print("B")
+	i=i+1
+else:
+   print("C")
+print("D")
+print(i) 	
